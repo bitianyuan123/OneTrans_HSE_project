644dda0dbb3547afc70b5fa10fc40bad8b03a2b4
Oleg Shvetsov <118572008+olezha223@users.noreply.github.com>
2026-05-27 02:02:27 +0300

    basic blocks for SASRec model

---
+++ onetrans/nn/ffn/ffn.py
@@ -0,0 +1,16 @@
+from torch import nn
+import torch
+
+class FFN(nn.Module):
+    def __init__(self, d_model: int, dropout: float = 0.0):
+        super().__init__()
+        dropout_p = dropout if self.training else 0.0
+        self.network = nn.Sequential(
+            nn.Linear(d_model, 4 * d_model, bias=False),
+            nn.GELU(),
+            nn.Dropout(p=dropout_p),
+            nn.Linear(4 * d_model, d_model, bias=False)
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.network(x)
