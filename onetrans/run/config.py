2d2c1f8c68ee73ab8e13f183f3185691c1b9ade3
Oleg Shvetsov <118572008+olezha223@users.noreply.github.com>
2026-06-04 15:45:55 +0300

    add database config

--- onetrans/run/config.py
+++ onetrans/run/config.py
@@ -12,3 +12,15 @@ class OneTransConfig:
 
     n_layers: int = 2
     n_heads: int = 2
+
+
+@dataclass
+class DatasetConfig:
+    dataset_type: str = 'flat'
+    dataset_size: str = '50m'
+    interaction_name: str = 'multi_event'
+    default_like_window_seconds: int = 24 * 60 * 60
+    lag_seconds: int = 15 * 60
+
+dataset_config = DatasetConfig()
+one_trans_config = OneTransConfig()
