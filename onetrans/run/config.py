2c352a3577e30f7d03b03dd14a1018df51149c7a
Oleg Shvetsov <118572008+olezha223@users.noreply.github.com>
2026-06-04 17:04:17 +0300

    add columns to config

--- onetrans/run/config.py
+++ onetrans/run/config.py
@@ -22,5 +22,27 @@ class DatasetConfig:
     default_like_window_seconds: int = 24 * 60 * 60
     lag_seconds: int = 15 * 60
 
+
+DENSE_COLUMNS: tuple[str, ...] = (
+    "user_lag_listen_cnt",
+    "user_lag_like_cnt",
+    "user_lag_full_play_cnt",
+    "user_lag_skip_cnt",
+    "item_lag_listen_cnt",
+    "item_lag_like_cnt",
+    "item_lag_full_play_cnt",
+    "item_lag_skip_cnt",
+    "ui_lag_listen_cnt",
+    "ui_lag_like_cnt",
+    "ui_lag_full_play_cnt",
+    "ui_lag_skip_cnt",
+    "user_lag_avg_played_ratio",
+    "item_lag_avg_played_ratio",
+    "ui_lag_avg_played_ratio",
+)
+MULTIVALENT_COLUMNS: tuple[str, ...] = ("artist_ids", "album_ids")
+SPARSE_COLUMNS = ("uid", "item_id")
+LABEL_COLUMNS = ("is_like", "is_full_play")
+
 dataset_config = DatasetConfig()
 one_trans_config = OneTransConfig()
