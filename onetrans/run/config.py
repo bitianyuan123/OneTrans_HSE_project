06dc3ebc54e085829406f23144501e983644f3a8
Grigory <grigory.chaykovsky@gmail.com>
2026-06-07 14:31:33 +0300

    SESSION GAP

--- onetrans/run/config.py
+++ onetrans/run/config.py
@@ -21,6 +21,7 @@ class DatasetConfig:
     interaction_name: str = 'multi_event'
     default_like_window_seconds: int = 24 * 60 * 60
     lag_seconds: int = 15 * 60
+    session_gap_seconds: int = 15 * 60
     batch_size : int = 8
     num_workers : int = 0
     max_users: int = None  # subsample N users; None = use all
